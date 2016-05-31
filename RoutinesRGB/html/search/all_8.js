40509fe9115dc172faa9b37f2a6a0810b390427d
Tim <timsee@users.noreply.github.com>
2016-05-30 20:40:23 -0400

    Version 1.9.6: Settings Page Update

--- RoutinesRGB/html/search/all_8.js
+++ RoutinesRGB/html/search/all_8.js
@@ -4,8 +4,8 @@ var searchData=
   ['setcustomcolorcount',['setCustomColorCount',['../a00006.html#gac0b1b6bae8d88290211aa095f9ae7879',1,'RoutinesRGB']]],
   ['setmaincolor',['setMainColor',['../a00006.html#gae780a47936a2411639a14bfccb8afd7c',1,'RoutinesRGB']]],
   ['singleblink',['singleBlink',['../a00007.html#ga5d430afe930ba13f4ad7a2254d0a3851',1,'RoutinesRGB']]],
-  ['singlefade',['singleFade',['../a00007.html#ga784971d65f5846ac2c87a3340a0b8949',1,'RoutinesRGB']]],
-  ['singleglimmer',['singleGlimmer',['../a00007.html#gafca211590303b12a4956dcd3fa0d9e4c',1,'RoutinesRGB']]],
+  ['singlefade',['singleFade',['../a00007.html#gac553795366d030f32c2808d109818b72',1,'RoutinesRGB']]],
+  ['singleglimmer',['singleGlimmer',['../a00007.html#ga7ba054273bc5f1b44deffbf2bb4e1645',1,'RoutinesRGB']]],
   ['single_20color_20routines',['Single Color Routines',['../a00007.html',1,'']]],
   ['singlesolid',['singleSolid',['../a00007.html#ga0fe5d4476e192ab9345b73974a58ffd6',1,'RoutinesRGB']]]
 ];
--- RoutinesRGB/html/search/functions_4.js
+++ RoutinesRGB/html/search/functions_4.js
@@ -4,7 +4,7 @@ var searchData=
   ['multibarsmoving',['multiBarsMoving',['../a00008.html#ga8e58804f0f3a08c4183590f5d3426f72',1,'RoutinesRGB']]],
   ['multibarssolid',['multiBarsSolid',['../a00008.html#gadf64ac1de541fde2606afa1da35593b7',1,'RoutinesRGB']]],
   ['multifade',['multiFade',['../a00008.html#ga6096f9640196d6bff7b033766971e3af',1,'RoutinesRGB']]],
-  ['multiglimmer',['multiGlimmer',['../a00008.html#ga199e9f5b52bc70146bf8c0a60645540d',1,'RoutinesRGB']]],
+  ['multiglimmer',['multiGlimmer',['../a00008.html#ga189c22dd3d944229c1827822eff6e050',1,'RoutinesRGB']]],
   ['multirandomindividual',['multiRandomIndividual',['../a00008.html#ga9f8210245eceeabfea41ddd46ea5f8e7',1,'RoutinesRGB']]],
   ['multirandomsolid',['multiRandomSolid',['../a00008.html#ga3944a78179df0e7950b86c9e01f58255',1,'RoutinesRGB']]]
 ];
